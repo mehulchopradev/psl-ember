f3a3c131969f96fb85690036a6f0bb83bff298d4
Mehul Chopra <mehul.chopra.dev@gmail.com>
2019-12-21 17:13:00 +0530

    one more commit

--- app/components/calc-editor.js
+++ app/components/calc-editor.js
@@ -1,4 +1,11 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    actions: {
+        onCalculate() {
+            const first = this.first;
+            const second = this.second;
+            debugger;
+        }
+    }
 });
--- tests/integration/components/calc-editor-test.js
+++ tests/integration/components/calc-editor-test.js
@@ -1,13 +1,38 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, pauseTest } from '@ember/test-helpers';
+import { render, pauseTest, fillIn, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+
+const template = hbs`<CalcEditor
+  @first={{first}}
+  @second={{second}}
+  @ops={{ops}}
+/>`
 
 module('Integration | Component | calc-editor', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    await render(hbs`<CalcEditor />`);
-    await pauseTest();
+  test('it renders with initial values', async function(assert) {
+    this.setProperties({
+      first: 6,
+      second: 3,
+      ops: '-',
+    });
+
+    await render(template);
+    assert.dom('.dum-first').hasValue('6');
+    assert.dom('.dum-second').hasValue('3');
+    assert.dom('.dum-ops').hasValue('-');
+  });
+
+  test('it performs calc', async function (assert) {
+    await render(template);
+
+    await fillIn('.dum-first', '10');
+    await fillIn('.dum-second', '4');
+    await fillIn('.dum-ops', '*');
+    await click('.dum-calc-btn');
+    
+    assert.dom('.dum-ans').hasValue('40');
   });
 });
